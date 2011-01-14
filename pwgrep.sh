278b1609ba5cf86c4d73d55e90911e4228c5b073
Paul Buetow <paul@buetow.org>
2011-01-14 12:10:16 +0000

    its now devel and not pre-X

--- pwgrep.sh
+++ pwgrep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-# pwgrep v0.8.1-pre-1 (c) 2009, 2010, 2011 by Paul Buetow
+# pwgrep v0.8.1-devel (c) 2009, 2010, 2011 by Paul Buetow
 # pwgrep helps you to manage all your passwords using GnuGP
 # for encryption and a versioning system (subversion by default)
 # for keeping track all changes of your password database. In
